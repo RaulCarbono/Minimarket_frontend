1e996e152400009f7c733a916d3f228c35906183
Raul Carbono <carbonoraul2@gmail.com>
2024-10-04 17:01:02 -0500

    minimarket basefront

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-
+import  Login  from "./components/Login"
 function App() {
  
 
   return (
     <>
-      <div>
-        Hola mundo
+      <div className='App'>
+        <Login/>
         </div>
     </>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from './login.module.css';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className={styles.container_card}>Login
+      <div className='card'>
+        <div className='card-left'></div>
+        <div className='card-rigth'>
+          <form action="">
+            <input type="text" placeholder='Email' />
+            <input type="text"  placeholder='Password'/>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
