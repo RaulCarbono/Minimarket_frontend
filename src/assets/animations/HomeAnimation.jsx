90df98b65f035594738901c0080e65515ed43e0a
Raul Carbono <carbonoraul2@gmail.com>
2024-10-07 15:07:44 -0500

    styles login and access

--- src/assets/animations/HomeAnimation.jsx
+++ src/assets/animations/HomeAnimation.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import animation from "./animation.json"
-import { LottiePlayer } from 'lottie-react'
+import Lottie from 'lottie-react'
 
 function HomeAnimation() {
   return (
     <div>
-        <LottiePlayer src={animation} alt=""
-        autoplay  
-        loop
-        style={{ height: '300px', width: '300px' }}
+        <Lottie animationData={animation}
         />
     </div>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './login.module.css';
 import HomeAnimation from '../assets/animations/HomeAnimation';
+import Google from '../assets/google.svg'
+import Facebook from '../assets/facebook.svg'
+import Linkedin from '../assets/linkedin.svg'
+
 
 const Login = () => {
   return (
@@ -9,12 +13,27 @@ const Login = () => {
         <div className={styles.card_left}>
           <HomeAnimation />
         </div>
-        <div className={styles.card_right}>
+        <div className={styles.container_card_right}>
+        
+        <div className={styles.title_Card}>Welcome Minimarket</div>
+        <div className={styles.container_options}>
+          <button>Login</button>
+          <button>Sign in</button>
+        </div>
+        <div className={styles.card_right}>   
           <form action="" className={styles.form_login}>
             <input type="text" className={styles.input_email} placeholder='Email' />
             <input type="text" className={styles.password_email}  placeholder='Password'/>
-            <button>Ingresar</button>
+            <div className={styles.container_button}>
+            <button className={styles.button_ingresar} type='button'>Ingresar</button>
+              </div> 
+              <div className={styles.external_button_container}>
+                  <div className=''><img src={Google} className={styles.icons} /></div>
+                  <div><img src={Facebook} alt="" className={styles.icons} /></div>
+                  <div><img src={Linkedin} alt="" className={styles.icons} /></div>
+              </div>
           </form>
+        </div>
         </div>
       </div>
     </div>
